f967033eaddaae83d8dfc851a14b54c81023fd6a
soy-curd <soycurd1@gmail.com>
2015-11-01 22:37:06 +0900

    Change text to goodbye.

--- app/analyze/analyze.py
+++ app/analyze/analyze.py
@@ -5,7 +5,7 @@ import json
 
 def main():
     
-    with open("./ningen.txt", "r") as f1:
+    with open("./goodbye.txt", "r") as f1:
         text = f1.read()
     
     # 1ページの文字数を500文字と仮定
@@ -18,7 +18,7 @@ def main():
     for x in range(page_num):
         init = x * pseud_char_num_per_page
         text_of_page = text[init: init + pseud_char_num_per_page]
-        texts.append({ "index": { "_index": "dazai-demo", "_type": "novel", "_id": x + 1 } })
+        texts.append({ "index": { "_index": "dazai-demo", "_type": "syayou", "_id": x + 1 } })
         texts.append({'id':x + 1, 'text':text_of_page, 'page':x})
     
     bulk = ""
